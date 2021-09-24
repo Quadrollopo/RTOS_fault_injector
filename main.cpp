ba47f0ec537500806518ff8945c94c11b847ed5f
Quadrollopo <davide97@gmail.com>
2021-09-24 20:08:45 +0200

    Working flip-bit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 int main(int argc, char** argv) {
@@ -19,8 +20,9 @@ int main(int argc, char** argv) {
     long startAddr = -1, endAddr;
     while (getline(mapfile, line) && mapfile.good()){
         if(line.find("heap") != string::npos) {
-            startAddr = stol(line.substr(0, 8));
-            endAddr = stol(line.substr(9, 8));
+            size_t separetor = line.find('-');
+            startAddr = stol(line.substr(0, separetor), nullptr, 16);
+            endAddr = stol(line.substr(separetor+1, separetor), nullptr, 16);
             perms = line.substr(line.find(' '), 4);
             break;
         }
@@ -34,17 +36,24 @@ int main(int argc, char** argv) {
         cout << "You dont have permission to write/read" << endl;
         return -4;
     }
-    fstream memFile("test", ios::binary | ios::in | ios::out);
-    memFile.open("/proc/" + pid + "/mem");
+//    fstream memFile("test", ios::binary | ios::in | ios::out);
+    fstream memFile("/proc/" + pid + "/mem", ios::binary | ios::in | ios::out);
     if(!memFile.is_open()){
-        //cout << "Can't open file /proc/" + pid + "/mem" << endl;
+        cout << "Can't open file /proc/" + pid + "/mem" << endl;
         return -5;
     }
-    memFile.seekg(startAddr, ios::beg);
-    char bit;
+    memFile.seekg(startAddr);
+    uint8_t bit, mask = 1;
+    bit = memFile.peek();
+//    memFile.get(bit);
 
-    memFile.get(bit);
-    cout << bit + ' ' + (bit & 1);
+    cout << "original bit:" << endl;
+    cout << bitset<8>(bit) << endl;
+    memFile.put(bit ^ mask);
+    memFile.seekg(startAddr);
+    cout << "modified bit:" << endl;
+    bit = memFile.peek();
+    cout << bitset<8>(bit) << endl;
     memFile.close();
 
     return 0;
