e1a7171d5a8e5d8197da1c577ff8f2cbf285b0db
Quadrollopo <davide97@gmail.com>
2021-10-21 18:34:26 +0200

    Fixes and add reading of multiple address from heap

--- main.cpp
+++ main.cpp
@@ -28,26 +28,32 @@ int main(int argc, char** argv) {
         return 2;
     }
     string line, perms;
-    long startAddr = -1, endAddr;
+    long address[100][2];
+    int numAddr = 0;
     while (getline(mapfile, line) && mapfile.good()){
         if(line.find("heap") != string::npos) {
-            size_t separator = line.find('-');
-            startAddr = stol(line.substr(0, separator), nullptr, 16);
-            endAddr = stol(line.substr(separator + 1, separator), nullptr, 16);
-            perms = line.substr(line.find(' '), 4);
+            while(getline(mapfile, line) && mapfile.good() && line.find("/usr") == string::npos) {
+                perms = line.substr(line.find(' '), 4);
+                if (perms.find('w') == string::npos || perms.find('r') == string::npos)
+                    continue;
+                size_t separator = line.find('-');
+                address[numAddr][0] = stol(line.substr(0, separator), nullptr, 16);
+                address[numAddr][1] = stol(line.substr(separator + 1, separator), nullptr, 16);
+                numAddr++;
+            }
             break;
         }
     }
     mapfile.close();
-    cout << (endAddr - startAddr) / 1024 << "KB di heap allocato" << endl;
-    if(startAddr == -1) {
+//    cout << (endAddr - startAddr) / 1024 << "KB di heap allocato" << endl;
+    if(numAddr == 0) {
         cout << "Heap not found" << endl;
         return 3;
     }
-    if(perms.find('w') == string::npos && perms.find('r') == string::npos){
-        cout << "You dont have permission to write/read" << endl;
-        return 4;
-    }
+//    if(perms.find('w') == string::npos || perms.find('r') == string::npos){
+//        cout << "You dont have permission to write/read" << endl;
+//        return 4;
+//    }
 //    fstream memFile("test", ios::binary | ios::in | ios::out);
     fstream memFile("/proc/" + pid + "/mem", ios::binary | ios::in | ios::out);
     if(!memFile.is_open()){
@@ -55,24 +61,28 @@ int main(int argc, char** argv) {
         return -5;
     }
     random_device generator;
-    uniform_int_distribution<long> addressDistribution(startAddr, endAddr);
+    uniform_int_distribution<int> range_distribution(0,numAddr-1);
     uniform_int_distribution<int> bit_distribution(0,7);
-    bool flag = false;
-    for (int i = 0; i < 60; ++i) {
+    for (int i = 0; i < 10000; ++i) {
         uint8_t byte, mask = bit_distribution(generator);
+        int index = range_distribution(generator);
+        uniform_int_distribution<long> address_distribution(address[index][0], address[index][1]);
+//        uniform_int_distribution<long> address_distribution(stol("ff9270", nullptr, 16), stol("ff927f", nullptr, 16));
         long addr;
         do {
-            addr = addressDistribution(generator);
+            addr = address_distribution(generator);
+//            addr = stol("ff9270", nullptr, 16);
             memFile.seekg(addr);
             byte = memFile.peek();
-        } while(byte == 0 || byte == 165);
-        if(byte == 255)
-            if(flag)
-                break;
-            else
-                flag = true;
-        else
-            flag = false;
+        } while (false);
+
+//        if(byte == 255)
+//            if(flag)
+//                break;
+//            else
+//                flag = true;
+//        else
+//            flag = false;
         //Flipbit
         byte ^= 1 << mask;
 
