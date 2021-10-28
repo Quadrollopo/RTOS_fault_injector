2c61e594f4acca5bac7e33103793382e013ff14a
Quadrollopo <davide97@gmail.com>
2021-10-28 18:27:11 +0200

    Converted the file in father process, changed injector. This is just a template

--- main.cpp
+++ main.cpp
@@ -5,96 +5,71 @@
 #include <random>
 #include <thread>
 #include <chrono>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define COLOR_RED     "\x1b[31m"
 #define COLOR_RESET   "\x1b[0m"
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    string pid;
-    if(argc == 1){
-        ifstream pidFile("rtos_pid.txt");
-        getline(pidFile, pid);
-        pidFile.close();
-    }
-    else {
-        pid = string(argv[1]);
-    }
-    ifstream mapfile;
-    mapfile.open("/proc/" + pid + "/maps");
-    if(!mapfile.is_open()){
-        cout << "Can't open file /proc/{}/maps" << endl;
-        return 2;
-    }
-    string line, perms;
-    long address[100][2];
-    int numAddr = 0;
-    while (getline(mapfile, line) && mapfile.good()){
-        if(line.find("heap") != string::npos) {
-            while(getline(mapfile, line) && mapfile.good() && line.find("/usr") == string::npos) {
-                perms = line.substr(line.find(' '), 4);
-                if (perms.find('w') == string::npos || perms.find('r') == string::npos)
-                    continue;
-                size_t separator = line.find('-');
-                address[numAddr][0] = stol(line.substr(0, separator), nullptr, 16);
-                address[numAddr][1] = stol(line.substr(separator + 1, separator), nullptr, 16);
-                numAddr++;
-            }
-            break;
-        }
-    }
-    mapfile.close();
-//    cout << (endAddr - startAddr) / 1024 << "KB di heap allocato" << endl;
-    if(numAddr == 0) {
-        cout << "Heap not found" << endl;
-        return 3;
-    }
-//    if(perms.find('w') == string::npos || perms.find('r') == string::npos){
-//        cout << "You dont have permission to write/read" << endl;
-//        return 4;
-//    }
-//    fstream memFile("test", ios::binary | ios::in | ios::out);
-    fstream memFile("/proc/" + pid + "/mem", ios::binary | ios::in | ios::out);
+int injector(pid_t pid, long startAddr, long endAddr) {
+    fstream memFile("/proc/" + to_string(pid) + "/mem", ios::binary | ios::in | ios::out);
     if(!memFile.is_open()){
-        cout << "Can't open file /proc/" + pid + "/mem" << endl;
-        return -5;
+        cout << "Can't open file /proc/" + to_string(pid) + "/mem" << endl;
+        return -1;
     }
     random_device generator;
-    uniform_int_distribution<int> range_distribution(0,numAddr-1);
     uniform_int_distribution<int> bit_distribution(0,7);
-    for (int i = 0; i < 10000; ++i) {
-        uint8_t byte, mask = bit_distribution(generator);
-        int index = range_distribution(generator);
-        uniform_int_distribution<long> address_distribution(address[index][0], address[index][1]);
-//        uniform_int_distribution<long> address_distribution(stol("ff9270", nullptr, 16), stol("ff927f", nullptr, 16));
-        long addr;
-        do {
-            addr = address_distribution(generator);
-//            addr = stol("ff9270", nullptr, 16);
-            memFile.seekg(addr);
-            byte = memFile.peek();
-        } while (false);
+    uint8_t byte, mask = bit_distribution(generator);
+    uniform_int_distribution<long> address_distribution(startAddr, endAddr);
 
-//        if(byte == 255)
-//            if(flag)
-//                break;
-//            else
-//                flag = true;
-//        else
-//            flag = false;
-        //Flipbit
-        byte ^= 1 << mask;
+    long addr = address_distribution(generator);
+    memFile.seekg(addr);
+    byte = memFile.peek();
 
-        memFile.seekg(addr);
-        memFile.put((char)byte);
+    //Flipbit
+    byte ^= 1 << mask;
+    memFile.put((char)byte);
 
-        cout << "Modified " <<
-        // put red color for the bit flipped
-        bitset<8>(byte).to_string().insert(8 - mask, COLOR_RESET).insert(7 - mask, COLOR_RED)
-        << " at 0x" << hex << addr << endl;
-        this_thread::sleep_for(chrono::milliseconds(500));
-    }
+    cout << "Modified " <<
+         // put red color for the bit flipped
+         bitset<8>(byte).to_string().insert(8 - mask, COLOR_RESET).insert(7 - mask, COLOR_RED)
+         << " at 0x" << hex << addr << endl;
     memFile.close();
     return 0;
 }
+
+void rtos(){}
+
+
+int main(int argc, char** argv){
+    pid_t pid_golden, pid_injector, pid_rtos;
+    pid_golden = fork();
+    if(pid_golden == 0) {
+        rtos(); //golden
+        return 0;
+    }
+    int status;
+    waitpid(pid_golden, &status, 0);
+
+    pid_rtos = fork();
+    if(pid_rtos == 0){
+        rtos();
+        return 0;
+    }
+    long startAddr, endAddr;
+
+    //TODO: Select a random range of addres to inject
+
+
+    pid_injector = fork();
+    if(pid_injector == 0){
+        injector(pid_rtos, startAddr, endAddr);
+        return 0;
+    }
+
+    //TODO: Check the output with the golden
+}
+
+
