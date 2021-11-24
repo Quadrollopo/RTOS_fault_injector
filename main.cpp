d170b4b91739eb8337329bd769679a9d80997725
asin27 <sindoniandrea8@gmail.com>
2021-11-24 10:26:00 +0100

    adding the possibility to run multiple instances

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <cstdlib>
+#include <limits>
 
 #define COLOR_RED     "\x1b[31m"
 #define COLOR_RESET   "\x1b[0m"
 
 using namespace std;
-
+/*
 int injector(pid_t pid, long startAddr, long endAddr) {
 	cout << "Child starting injector" << endl;
 	ifstream mapfile;
@@ -76,6 +77,33 @@ int injector(pid_t pid, long startAddr, long endAddr) {
 	}
 	memFile.close();
 	return 0;
+}*/
+int injector(pid_t pid, long startAddr, long endAddr) {
+    cout << "Child starting injector" << endl;
+    fstream memFile("/proc/" + to_string(pid) + "/mem", ios::binary | ios::in | ios::out);
+    if (!memFile.is_open()) {
+        cout << "Can't open file /proc/" + to_string(pid) + "/mem" << endl;
+        return -1;
+    }
+    random_device generator;
+    uniform_int_distribution<int> bit_distribution(0, 7);
+    uint8_t byte, mask = bit_distribution(generator);
+    uniform_int_distribution<long> address_distribution(startAddr, endAddr);
+
+    long addr = address_distribution(generator);
+    memFile.seekg(addr);
+    byte = memFile.peek();
+
+    //Flipbit
+    byte ^= 1 << mask;
+    memFile.put((char) byte);
+
+    cout << "Modified " <<
+         // put red color for the bit flipped
+         bitset<8>(byte).to_string().insert(8 - mask, COLOR_RESET).insert(7 - mask, COLOR_RED)
+         << " at 0x" << hex << addr << endl;
+    memFile.close();
+    return 0;
 }
 
 void rtos() {
@@ -85,18 +113,31 @@ void rtos() {
 	//    "../build/freeRTOS");
 }
 
-int checkFiles() {
-	ifstream golden_output("../Golden_execution.txt");
-	ifstream rtos_output("../Falso_Dante.txt");
+long getFileLen(ifstream &file){
+    file.ignore( std::numeric_limits<std::streamsize>::max() );
+    std::streamsize length = file.gcount();
+    file.clear();   //  Since ignore will have set eof.
+    file.seekg( 0, std::ios_base::beg );
+    return (long) length;
+}
+
+int checkFiles(int pid_golden, int pid_rtos) {
+	ifstream golden_output("../files/Golden_execution" + to_string(pid_golden) + ".txt");
+	ifstream rtos_output( "../files/Falso_Dante_" + to_string(pid_rtos) + ".txt");
 	bool found = false;
+    long s1, s2;
 	if (!golden_output.is_open()) {
 		cout << "Can't open the golden execution output" << endl;
 		return -1;
 	}
 	if (!rtos_output.is_open()) {
+        cout << pid_rtos << endl;
 		cout << "Can't open the rtos execution output" << endl;
 		return -1;
 	}
+    s1 = getFileLen(golden_output);
+    s2 = getFileLen(rtos_output);
+
 	for (string g_line, f_line; getline(golden_output, g_line), getline(rtos_output, f_line);) {
 		if (g_line != f_line) {
 			found = true;
@@ -105,7 +146,10 @@ int checkFiles() {
 		}
 	}
 	if (!found)
-		cout << endl << "No differences has been found" << endl;
+		cout << endl << "No differences have been found" << endl;
+    if(s1!=s2)
+        cout << endl << "Files differ in size" << endl << "golden = " << s1 << "; falso = " << s2 << endl;
+
 	rtos_output.close();
 	golden_output.close();
 	return 0;
@@ -116,7 +160,7 @@ int main(int argc, char **argv) {
 	int status, status2;
 	int chosen;
 	if (argc < 2)
-		chosen = 0;
+		chosen = 1;
 	else
 		chosen = (int) strtol(argv[1], nullptr, 10);
 
@@ -129,7 +173,8 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 	waitpid(pid_golden, &status, 0);
-	system("mv ../Falso_Dante.txt ../Golden_execution.txt");
+    const string cmd = "mv ../files/Falso_Dante_" + to_string(pid_golden) + ".txt ../files/Golden_execution" + to_string(pid_golden) + ".txt";
+	system((const char *) cmd.c_str());
 
 	int iter = 0;
 	while (iter < 8) {
@@ -137,31 +182,51 @@ int main(int argc, char **argv) {
 		pid_rtos = fork();
 		if (pid_rtos == 0) {
 			cout << "freeRTOS iter : " << iter << endl;
-			//this_thread::sleep_for(chrono::seconds(1));
+			this_thread::sleep_for(chrono::milliseconds (500));
 			rtos();
 			return 0;
 		}
-
 		long startAddr = 0x431000, endAddr = 0x432000;
-		long addresses[4] = {0x433ba0, 0x433ba8, 0x4316e8, 0x4316f0};
+		//long addresses[6] = {0x433ba0, 0x433ba8, 0x4316e8, 0x4316f0, 0x431700, 0x4311f8};
+        string variables[7] = {"xNextTaskUnblockTime", "xIdleTaskHandle", "xTickCount", "xSchedulerRunning", "xYieldPending", "uxTaskNumber", "xIdleTaskHandle"};
+        long addrOS[7] = {0x431a28, 0x431a30, 0x4319f0, 0x431a00, 0x431a10, 0x431a20, 0x431a30};
+        long addr = 0x316e8;
 
-		//TODO: Select a random range of address to inject
+        //TODO: Select a random range of address to inject
 
 		pid_injector = fork();
 		if (pid_injector == 0) {
-			this_thread::sleep_for(chrono::milliseconds(rand() % 9000));
-			//injector(pid_rtos, startAddr, endAddr);
-			injector(pid_rtos, addresses[chosen], addresses[chosen] + 8);
-			return 0;
+            //ptrace(PTRACE_SEIZE, pid_rtos, NULL, NULL);
+            this_thread::sleep_for(chrono::milliseconds(rand() % 7000));
+			//injector(pid_rtos, startAddr - 0x400000, endAddr - 0x400000);
+            switch(chosen){
+                case 0:
+                    injector(pid_rtos, 0x431320, 0x431320 + 176); //TCB1
+                    break;
+                case 1:
+                    injector(pid_rtos, 0x431620, 0x431620 + 176); //TCB2
+                    break;
+                case 2:
+                    injector(pid_rtos, 0x4313e0, 0x4313e0 + 576); //TCB3
+                    break;
+                case 3:
+                    injector(pid_rtos, 0x431760, 0x431760 + 16);
+                    break;
+                default:
+                    break;
+            }
+			//injector(pid_rtos, 0x431320, 0x431320 + 256);
+            //injector(pid_rtos, addrOS[chosen], addrOS[chosen]+8);
+            return 0;
 		}
 		waitpid(pid_rtos, &status, 0);
 		waitpid(pid_injector, &status2, 0);
-		/*
-		string cmd = "diff ../Golden_execution.txt ../Falso_Dante.txt >> ../diffs/diff" + to_string(iter) + ".txt";
+
+		/*string cmd = "diff ../Golden_execution.txt ../Falso_Dante.txt >> ../diffs/diff" + to_string(iter) + ".txt";
 		cout << endl << "Now printing differences between generated files" << endl;
 		system(cmd.c_str());
-		cout << "print done" << endl; */
-		checkFiles();
+		cout << "print done" << endl;*/
+		checkFiles(pid_golden, pid_rtos);
 		iter++;
 	}
 
