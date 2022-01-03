7b844d25a1325d650c5a39dca31ff605bb20455a
asin27 <sindoniandrea8@gmail.com>
2022-01-03 12:05:05 +0100

    adding gdb folder

---
+++ gdb_stuff/getaddr.py
@@ -0,0 +1,31 @@
+f = open("gdb.output", "r")
+
+addr = []
+names = []
+sizes = []
+
+s = f.readline()
+n = f.readline()
+while s!='':
+	if names.count(s.split(' ')[1].split('\t')[0][1:-2]) == 0:
+		addr.append(s.split(' ')[0])
+		names.append(s.split(' ')[1].split('\t')[0][1:-2])
+		sizes.append(n.split(' = ')[1].strip())
+	s = f.readline()
+	n = f.readline()
+
+f.close()
+
+s = ""
+
+for i in range(len(addr)):
+	if i >=27:
+		s+="{\"" +  names[i] + "\", " + addr[i] + ", " + sizes[i] + ", true}, "
+	else:
+		s+="{\"" +  names[i] + "\", " + addr[i] + ", " + sizes[i] + ", false}, "
+	if names.count(names[i]) > 1:
+		print(names[i])
+f.close()
+
+print(s)
+print(len(addr))
